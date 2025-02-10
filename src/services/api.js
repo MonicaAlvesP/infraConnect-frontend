0309e9603a00fe45db17a252a74edb0805e50210
MonicaAlvesP <monica52782@gmail.com>
2025-02-10 12:07:15 -0300

    submodulo frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'https://infraconnect-api.fly.dev/'
+const API_URL = `process.env.REACT_APP_API_URL` || "http://localhost:8000";
 
 const api = axios.create({
   baseURL: API_URL,
